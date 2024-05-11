72bda66f60c539c393c8495cc28e6e6c00699ab0
Sahil Sinha <115421224+SahilSinha22@users.noreply.github.com>
2024-05-11 10:29:34 +0530

    Update jouney.js

--- component/jouney.js
+++ component/jouney.js
@@ -4,7 +4,7 @@ const jouney = () => {
   return (
     <div className='flex flex-col justify-center  py-5 items-center'>
       <div className="mx-2">
-      <h1 className=' xl:text-6xl md:text-5xl my-10 capitalize font-bold font-poppins text-3xl text-center dark:text-white'>
+      <h1 className=' xl:text-6xl md:text-5xl my-10 capitalize font-bold font-poppins text-3xl text-center '>
         <span className='text-blue-500'>Spacetotech</span> Journey of Mobile App Development
       </h1>
       </div>
