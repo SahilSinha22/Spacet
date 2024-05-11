d9c561e9086d306653b8ee5f9765755486e045a2
Sahil Sinha <115421224+SahilSinha22@users.noreply.github.com>
2024-05-11 10:26:41 +0530

    Update Certi.js

--- component/Certi.js
+++ component/Certi.js
@@ -4,7 +4,7 @@ const Certi = () => {
   return (
     <div className=" flex flex-col   py-5  items-center">
       <div className=" font-poppins text-center  ">
-        <h1 className="text-blue-500   xl:text-6xl  mt-10 capitalize font-bold font-poppins text-3xl md:text-6xl text-center dark:text-white">Certifications</h1>
+        <h1 className="text-blue-500   xl:text-6xl  mt-10 capitalize font-bold font-poppins text-3xl md:text-6xl text-center ">Certifications</h1>
         <p className="xl:text-2xl md:text-2xl 2xl:text-2xl text-black text-sm  sm:text-xl text-center justify-center px-5 py-5">
           {" "}
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, sunt
