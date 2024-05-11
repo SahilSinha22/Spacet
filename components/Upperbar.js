3237a96e6deb7875e231c2f96e2aad5e042608d9
Sahil Sinha <115421224+SahilSinha22@users.noreply.github.com>
2024-05-11 10:38:22 +0530

    Update Upperbar.js

--- components/Upperbar.js
+++ components/Upperbar.js
@@ -3,12 +3,12 @@ import React from "react";
 import Image from "next/image";
 const bar = () => {
   return (
-    <nav className=" text-xs  h-auto  md:w-auto xl:w-auto xl:p-0 xl:m-0    2xl:justify-end   xl:justify-end bg-blue-600  sm:w-auto sm:p-0 sm:justify-start  sm:font-small sm:flex-col  flex flex-row border-gray-200 dark:bg-gray-900  max-h-14 items-center justify-end  ">
+    <nav className=" text-xs  h-auto  md:w-auto xl:w-auto xl:p-0 xl:m-0    2xl:justify-end   xl:justify-end bg-blue-600  sm:w-auto sm:p-0 sm:justify-start  sm:font-small sm:flex-col  flex flex-row border-gray-200   max-h-14 items-center justify-end  ">
       <div className=" md:justify-end 2xl:ml-40 p-2 sm:p-2 xl:mr-0  2xl:mr-0 xl:text-lg 2xl:text-2xl flex flex-row justify-end   2xl:justify-end  lg:justify-end xl:justify-end  md:items-center sm:justify-start   sm:text-xs all sm:mt-1  sm:ml-1 sm:text-start  sm:items-center  sm:flex-col items-center mr-0 max-w-screen-xl    ">
         <div className="    text-xs xl:text-lg xl:pr-0  xl:mr-0  xl:p-0 xl:m-0 2xl:text-2xl flex flex-row  sm:ml-8 2xl:justify-end items-center xl:justify-end lg:justify-end md:w-full md:justify-end  sm:pl-0 lg:mx-2 sm:m-0 sm:text-xs  rtl:space-x-reverse ">
           <a
             href="#"
-            className="lg:text-xl text-xs xl:text-xl 2xl:text-2xl    md:mx-2   sm:pl-0 sm:left-0 sm:text-xs sm:ml-0 mb-2 text-gray-200  dark:text-blue-500 hover:underline"
+            className="lg:text-xl text-xs xl:text-xl 2xl:text-2xl    md:mx-2   sm:pl-0 sm:left-0 sm:text-xs sm:ml-0 mb-2 text-gray-200   hover:underline"
           >
             Feature Listing
           </a>
@@ -16,7 +16,7 @@ const bar = () => {
           <Image src="FlagIndia.svg" className=" h-3 sm:h-5 mr-1 md:mx-2  mb-2" alt="logo"  width={50} height={5} />{" "}
           <a
             href="tel:5541251234"
-            className="  text-xs  xl:text-lg 2xl:text-2xl mx-2 mb-2 lg:text-xl sm:text-xs  md:mx-2  text-gray-200 dark:text-white hover:underline"
+            className="  text-xs  xl:text-lg 2xl:text-2xl mx-2 mb-2 lg:text-xl sm:text-xs  md:mx-2  text-gray-200  hover:underline"
           >
             {" "}
             +91 88820076755
@@ -26,7 +26,7 @@ const bar = () => {
           <Image src="Flag.svg" className="h-3 sm:h-5 mx-1 md:mx-2 mb-2" alt="" width={50} height={5} />
           <a
             href="tel:5541251234"
-            className=" text-xs   xl:text-lg 2xl:text-2xl  mb-2 lg:text-xl  md:mx-2 sm:text-xs text-gray-200 dark:text-white hover:underline"
+            className=" text-xs   xl:text-lg 2xl:text-2xl  mb-2 lg:text-xl  md:mx-2 sm:text-xs text-gray-200  hover:underline"
           >
             +971-50-694-3266
           </a>
